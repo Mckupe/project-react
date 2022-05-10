f7091810f0dda06c77a854273db09f84d4fe5f82
mckupe <nohrin09072001@yandex.ru>
2022-05-10 22:11:40 +0500

    small fixes

--- src/pages/feedback Api.js
+++ src/pages/feedback Api.js
@@ -1,11 +1,13 @@
-async function postData(url = '') {
+async function postData(url = 'Url моего будующего сервера') {
+    const values = [].slice.call(document.getElementsByClassName("inpField")).map((n) => {return n.value})
+    console.log('try to post massage from : ' + values);
     try {
         fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(document.getElementsByClassName("inpField").value)
+            body: JSON.stringify(values)
         });
     } catch (err) {
         console.log(err);
