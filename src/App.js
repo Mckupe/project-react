b0dd03c2e3a15955a43229a242571671c79935f1
mckupe <nohrin09072001@yandex.ru>
2022-05-10 17:42:00 +0500

    Диплом. Верстка

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes,Route} from 'react-router-dom';
+
+import {TestPage} from './pages/Test'
+import {Main} from './pages/Main'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div class="App">             
+            <Routes>
+                <Route exact path="/" element={<Main/>}/>
+                <Route exact path="/test" element={<TestPage/>}/>
+            </Routes>
+        </div>
   );
 }
 
